fc09dd837eda207eac24734ad8307e92d018dfa4
A-CHRI <alexander.anker.chr@gmail.com>
2022-10-15 23:05:33 +0200

    Added exercises

--- src/stack.c
+++ src/stack.c
@@ -1,25 +1,100 @@
 #include "stack.h"
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-void initialize(stack* s){
-  //implement initialize here
-}
-
-void push(int x, stack* s){
-    //implement push here
-}
-
-int pop(stack* s){
-    // implement pop here
-  return -1;
-}
-
-bool empty(stack* s)
+typedef struct node
 {
-  //implement empty here
-  return false;
+    int data;
+    struct node *next;
+} node;
+
+void Initialize(node **top) {
+    /* Set the top pointer to NULL */
+    *top = NULL;
 }
 
-bool full(stack* s) {
-    //implement full here
-  return false;
+void Push(int x, node **top)
+{
+    /* Pre: Non-full stack */
+
+    /* Post: Add element x to the top/front of the list */
+    node *new = (node *)malloc(sizeof(node)); // Allocate memory for the node
+
+    new->data = x;
+    new->next = *top;
+
+    /* Set the top node as the newly added node */
+    *top = new;
+}
+
+int Pop(node **top)
+{
+    /* Pre: Non-empty stack */
+    assert(*top != NULL);
+
+    /* Post: Free the top node, and return its value */
+    node *t = *top;
+    *top = (*top)->next;
+
+    /* Pull the data from the node, then free it*/
+    int temp = t->data;
+    free(t);
+
+    /* Return the data */
+    return temp;
+}
+
+bool Empty(node **top)
+{
+    /* Post: Return TRUE if the top node is NULL*/
+    return *top == NULL;
+}
+
+void Display(node **top)
+{
+    /* Pre: Non-empty stack */
+    assert(*top != NULL);
+
+    /* Post: Print each element of the stack in order */
+    node *t = *top;
+    while (t != NULL)
+    {
+        printf("%d ", t->data);
+        t = t->next;
+    }
+    printf("\n");
+}
+
+int main(void)
+{
+    /* Initialise the stack */
+    node *s;
+    Initialize(&s);
+    // Initialize(s);
+
+    /* TEST A: After initialization the stack must be empty */
+    if (Empty(&s))
+        printf("The stack is empty after initialization.\n");
+    else
+        printf("The stack is NOT empty!\n");
+
+    /* Push some elements to the stack */
+    Push(1, &s);
+    Push(2, &s);
+
+    /* TEST B: After pushing an element to the stack and popping, the stack must be the same */
+    Display(&s);
+    Push(3, &s);
+    Pop(&s);
+    Display(&s);
+
+    /* TEST C: After pushing two elements to the stack and popping twice, the two elements should be correctly distributed */
+    Push(10, &s);
+    Push(20, &s);
+    printf("First element popped, should be latest element pushed (20): %d\n", Pop(&s));
+    printf("Second element popped, should be second element pushed (10): %d\n", Pop(&s));
+
+    return 0;
 }
--- src/taylor_sine.c
+++ src/taylor_sine.c
@@ -1,7 +1,31 @@
 #include "taylor_sine.h"
+#include <math.h>
+#include <assert.h>
+
+double fact(double x)
+{
+    /* Pre: Non-negative integer */
+    assert(x > 0);
+
+    /* Post: Recursively compute the factorial of x */
+    if (x == 1)
+        return 1;
+    return x * fact(x - 1);
+}
+
 double taylor_sine(double x, int n)
 {
-    /* implement your function here */
-    
-    return -1.0;
+    /* Pre: Terms n, is a positive integer, but no greater than 12 */
+    assert(n > 0);
+
+    /* Post: Compute the taylor value of sine */
+    double r = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+            r -= pow(x, 2 * i - 1) / fact(2 * i - 1);
+        else
+            r += pow(x, 2 * i - 1) / fact(2 * i - 1);
+    }
+    return r;
 }
